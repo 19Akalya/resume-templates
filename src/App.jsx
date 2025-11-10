469bc2d221fee1a0f4627cc38800054913239f6a
Akalya Umapathi <akalyaumapathi19@gmail.com>
2025-11-10 22:23:04 +0530

    routes changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import './App.css'
 import { AdLoginPage } from "./Login.jsx"
 import { UsLoginPage } from './UserLogin'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from "./index.jsx"
-import AdDashboardpage from"./Dashboard.jsx"
+import AdDashboardpage from "./Dashboard.jsx"
 import AddNewDocterPage from './DoctorPage.jsx'
 import Userpanel from "./userpanel.jsx"
-import Adminpanel from"./adminpanel.jsx"
+import Adminpanel from "./adminpanel.jsx"
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-        
-        <Route path="/userlog" element={<UsLoginPage/>} />
-        <Route path="/adminlog" element={<AdLoginPage/>}/>
-        <Route path="/addashboard" element={<AdDashboardpage/>}/>
-        <Route index exact path="/" element={<Index/>}/>
-        <Route path="/addnewdocter" element={<AddNewDocterPage/>}/>
-        <Route path="/userpanel" element={<Userpanel/>}/>
-        <Route path="/adminpanel" element={<Adminpanel/>}/>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/user/login" element={<UsLoginPage />} />
+          <Route path="/admin/login" element={<AdLoginPage />} />
+          <Route path="/admin/dashboard" element={<AdDashboardpage />} />
+          <Route index exact path="/" element={<Index />} />
+          <Route path="/newdocter/add" element={<AddNewDocterPage />} />
+          <Route path="/user/panel" element={<Userpanel />} />
+          <Route path="/admin/panel" element={<Adminpanel />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
